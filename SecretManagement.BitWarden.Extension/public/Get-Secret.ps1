5127d1a591194c267bc859e9834ede5d44213f8e
wmarshall <wmarshall@industrialinfo.com>
2022-08-11 13:34:49 -0500

    Minor code tweaks and renaming of things. Added VSCode extension recomendations and more formatting settings.

--- SecretManagement.BitWarden.Extension/public/Get-Secret.ps1
+++ SecretManagement.BitWarden.Extension/public/Get-Secret.ps1
@@ -16,7 +16,7 @@ function Get-Secret {
     )
     # Enable Verbose Mode inside this script if passed from the wrapper.
     if($AdditionalParameters.ContainsKey('Verbose') -and ($AdditionalParameters['Verbose'] -eq $true)) {$script:VerbosePreference = 'Continue'}
-    $AdditionalParameters = Get-Defaults $AdditionalParameters
+    $AdditionalParameters = Merge-Defaults $AdditionalParameters
     Sync-BitwardenVault $AdditionalParameters.ResyncCacheIfOlderThan
 
     [System.Collections.Generic.List[string]]$CmdParams = @("get","item")
