f8daa40a9cd6b8d880a09b505f803467ce44775d
wmarshall <62070150+marshallwp@users.noreply.github.com>
2022-08-11 13:34:49 -0500

    Minor code tweaks and renaming of things. Added VSCode extension recomendations and more formatting settings.

--- SecretManagement.BitWarden.Extension/private/Sync-BitwardenVault.ps1
+++ SecretManagement.BitWarden.Extension/private/Sync-BitwardenVault.ps1
@@ -5,18 +5,18 @@
     By locally caching the LastSyncedTime we can significanly reduce the number of `Invoke-BitwardenCLI sync --last` calls we need to make.  As these are very slow calls, caching the results can improve performance dramatically.
 .PARAMETER ResyncCacheIfOlderThan
     TimeSpan indicating how long before the bitwarden cache is considered expired and a new sync needs to be run.
-.PARAMETER CacheLocation
-    Where to store the cache file.  Defaults to a subdirectory of the SecretManagement vault registry location..
-    https://github.com/PowerShell/SecretManagement/#extension-vault-registry-file-location
 .PARAMETER Force
     If provided, the bitwarden cach will be synced regardless of how old it is.
 #>
 function Sync-BitwardenVault {
     Param(
         [TimeSpan]$ResyncCacheIfOlderThan,
-        [string]$CacheLocation = $IsWindows ? "$env:LocalAppData\Microsoft\PowerShell\secretmanagement\bitwarden-ext\LastSyncedTime.txt" : "$HOME/.secretmanagement/bitwarden-ext/LastSyncedTime.txt",
         [switch]$Force
     )
+
+    # Stores the cache file in a subdirectory of the SecretManagement vault registry location:
+    # https://github.com/PowerShell/SecretManagement/#extension-vault-registry-file-location
+    $CacheLocation = $IsWindows ? "$env:LocalAppData\Microsoft\PowerShell\secretmanagement\bitwarden-ext\LastSyncedTime.txt" : "$HOME/.secretmanagement/bitwarden-ext/LastSyncedTime.txt"
 
     # Get Last Synced time from file or the Bitwarden CLI (much slower)
     if(Test-Path -Path $CacheLocation -PathType Leaf) {
@@ -27,8 +27,8 @@ function Sync-BitwardenVault {
         $LastSyncedTime = Invoke-BitwardenCLI sync --last | Get-Date
     }
 
-    # Check if time since this time is greater than $ResyncCacheIfOlderThan time back.
-    if((New-TimeSpan -Start $LastSyncedTime).TotalSeconds -gt $ResyncCacheIfOlderThan.TotalSeconds -or $Force) {
+    # Sync Cache if $Force or if time since $LastSyncedTime is greater than $ResyncCacheIfOlderThan time back.
+    if($Force -or (New-TimeSpan -Start $LastSyncedTime).TotalSeconds -gt $ResyncCacheIfOlderThan.TotalSeconds) {
         # If so, sync the vault.
         Invoke-BitwardenCLI sync --quiet
         # And update the cache.
