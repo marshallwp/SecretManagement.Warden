de555c6a80104fc6bb86140c981429033f619731
wmarshall <wmarshall@industrialinfo.com>
2024-09-04 17:06:13 -0500

    Merge v1.1.3 into Main

---
+++ SecretManagement.Warden.Extension/test/private/Get-FullSecret.Tests.ps1
@@ -0,0 +1,26 @@
+BeforeAll {
+    $BasePath = Join-Path $PSScriptRoot ".." ".."
+    . (Join-Path $BasePath "private" "Get-FullSecret.ps1")
+    . (Join-Path $BasePath "private" "Merge-Defaults.ps1")
+    . (Join-Path $BasePath "private" "Sync-BitwardenVault.ps1")
+    . (Join-Path $BasePath "private" "Invoke-BitwardenCLI.ps1")
+}
+
+Describe "Get-FullSecret" {
+    BeforeAll {
+        Mock Sync-BitwardenVault { }
+    }
+    It "Returns '`$null' for non-existent secret." {
+        Mock Invoke-BitwardenCLI {
+            $ex = New-Object System.DirectoryServices.AccountManagement.NoMatchingPrincipalException "Not found."
+            Write-Error $ex -Category ObjectNotFound -ErrorAction Stop
+        }
+        Get-FullSecret -Name '00000000-0000-0000-0000-000000000000' | Should -BeNullOrEmpty
+    }
+    It "Returns Expected Secret Object" {
+        Mock Invoke-BitwardenCLI { return Get-Content (Join-Path $PSScriptRoot "example-secret" "example-secret.json") }
+        $expected = Import-Clixml -Path (Join-Path $PSScriptRoot "example-secret" "example-secret.xml")
+        $test = Get-FullSecret -Name '92662bb0-5339-4de3-a69f-65a536e93173'
+        Compare-Object -ReferenceObject $expected -DifferenceObject $test | Should -BeNullOrEmpty
+    }
+}
