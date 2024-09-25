2de90f7b2b41a426115c9d4a79be120d6b9408b0
wmarshall <wmarshall@industrialinfo.com>
2024-09-25 11:40:49 -0500

    Localize Warnings and Expand Testing

---
+++ SecretManagement.Warden.Extension/private/localization/es/Test-CLIVersion.psd1
@@ -0,0 +1,8 @@
+# Spanish
+@{
+    WarnOutdated        = "Su bitwarden-cli es la versión {0} y está desactualizada. Por favor, actualice al menos a la versión {1}."
+    WarnSpecificVersion = "Su bitwarden-cli es la versión {0}. Esta versión de la CLI tiene un problema conocido que afecta a [{1}], que es utilizado por [{2}]."
+    WarnVersionRange    = "Su bitwarden-cli es la versión {0}. Las versiones {0} - {1} del CLI tienen un problema conocido que afecta a [{2}], que es utilizado por [{3}]."
+    See                 = "Mira: {0}."
+    StrongAction        = "Se recomienda `e[3mencarecidamente`e[23m pasar a otra versión."
+}
---
+++ SecretManagement.Warden.Extension/test/private/Test-CLIVersion.Tests.ps1
@@ -0,0 +1,80 @@
+BeforeAll {
+    . (Join-Path $PSScriptRoot ".." ".." "private" "Test-CLIVersion.ps1")
+}
+
+Describe "Test-CLIVersion" {
+    BeforeAll {
+        [Version]$CurrentVersion = '2024.9.0'
+        [Version]$NewerVersion   = '2024.10.0'
+        [Version]$OlderVersion   = '2024.8.0'
+        $direct = Import-CliXml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "direct.xml")
+        $bw_version = Import-CliXml (Join-Path $PSScriptRoot "mock-cli" "response" "bw-version.xml")
+    }
+    Context "General" {
+        It "Does nothing for valid version" {
+            Mock $direct -MockWith { return $bw_version }
+            Test-CLIVersion -BitwardenCLI $direct -MinSupportedVersion $OlderVersion -WarningVariable warn -WarningAction Ignore
+            $warn | Should -BeNullOrEmpty
+        }
+        It "Warn when CurrentVersion < MinSupportedVersion" {
+            Mock $direct -ParameterFilter {$args[0] -eq '--version'} -MockWith { return $bw_version }
+
+            Test-CliVersion -BitwardenCLI $direct -MinSupportedVersion $NewerVersion -WarningVariable warn -WarningAction Ignore
+            $warn | Should -Be "Your bitwarden-cli is version $CurrentVersion and is out of date. Please upgrade to at least version $NewerVersion."
+        }
+        # It "Warn when CurrentVersion On Blacklist" {
+
+        # }
+    }
+
+    Context "<Source>" -ForEach @(
+        @{Source='direct';From='cli';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "direct.xml"))},
+        @{Source='brew';From='pkg-mgr';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "brew.xml"))},
+        @{Source='choco';From='cli';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "choco.xml"))},
+        @{Source='npm';From='pkg-mgr';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "npm.xml"))},
+        @{Source='scoop';From='pkg-mgr';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "scoop.xml"))},
+        @{Source='snap';From='pkg-mgr';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "snap.xml"))},
+        @{Source='winget-machine';From='cli';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "winget-machine.xml"))},
+        @{Source='winget-user';From='cli';Mocked=(Import-Clixml (Join-Path $PSScriptRoot "mock-cli" "bw-info" "winget-user.xml"))}
+    ) {
+        BeforeEach {
+            if($From -eq "cli") {
+                Mock -CommandName $Mocked -ParameterFilter {$args[0] -eq "--version"} -MockWith { return $bw_version } -Verifiable
+            }
+            elseif ($From -eq "pkg-mgr") {
+                Mock -CommandName "Get-Command" -ParameterFilter {$Name -eq $Source} -MockWith { return $true }
+
+                switch($Source) {
+                    "brew"  {
+                        function brew { return $null }
+                        Mock -CommandName "brew" -ParameterFilter {$args[0] -eq "list" -and $args[1] -eq "bitwarden-cli" -and $args[2] -eq "--versions"} `
+                            -MockWith { return Import-Clixml -Path (Join-Path $PSScriptRoot "mock-cli" "response" "brew-list.xml")} -Verifiable
+                     }
+                    "npm"   {
+                        function npm { return $null }
+                        Mock -CommandName "npm" -ParameterFilter {$args[0] -eq "view" -and $args[1] -eq "-g" -and $args[2] -eq "@bitwarden/cli" -and $args[3] -eq "version"} `
+                            -MockWith { return Import-Clixml -Path (Join-Path $PSScriptRoot "mock-cli" "response" "npm-view.xml") } -Verifiable
+                    }
+                    "scoop" {
+                        function scoop { return $null }
+                        Mock -CommandName "scoop" -ParameterFilter {$args[0] -eq "list" -and $args[1] -eq "bitwarden-cli"} `
+                            -MockWith { return Import-CliXml -Path (Join-Path $PSScriptRoot "mock-cli" "response" "scoop-list.xml") } -Verifiable
+                    }
+                    "snap"  {
+                        function snap { return $null }
+                        Mock -CommandName "snap" -ParameterFilter {$args[0] -eq "list" -and $args[1] -eq "bw"} `
+                            -MockWith { return Import-CliXml -Path (Join-Path $PSScriptRoot "mock-cli" "response" "snap-list.xml") } -Verifiable
+                    }
+                }
+            }
+        }
+        It "Queries the <From> for its version when CurrentVersion < MinSupportedVersion" {
+            Test-CLIVersion -BitwardenCLI $Mocked -MinSupportedVersion $NewerVersion -WarningAction Ignore | Should -InvokeVerifiable
+        }
+        It "Does not run <From> query if file version is acceptable" {
+            # Change the Mocked version to be an acceptable one.  Only works because all mocks are deserialized objects.
+            $Mocked.Version = $CurrentVersion
+            Test-CLIVersion -BitwardenCLI $Mocked -MinSupportedVersion $OlderVersion -WarningAction Ignore | Should -Not -InvokeVerifiable
+        }
+    }
+}
